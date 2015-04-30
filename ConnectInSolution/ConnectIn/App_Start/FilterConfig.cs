f2edf206c0401ac3c406d5416c4e46190b34f560
Davíð Guðni <davidgh13@ru.is>
2015-04-30 11:10:24 -0700

    Initial commit, for this repository. This project is ASP .NET MVC with unit tests, indvidual authentication and not stored in the cloud

---
+++ ConnectInSolution/ConnectIn/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ConnectIn
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ConnectInSolution/ConnectIn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConnectIn.Startup))]
+namespace ConnectIn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
