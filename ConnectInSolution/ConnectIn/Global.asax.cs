37bf54ee1cf706ccfcec0e48278be4053bfe1e79
Davíð Guðni <davidgh13@ru.is>
2015-05-03 09:07:59 -0700

    Unfinished changes to fix initilizerclass

--- ConnectInSolution/ConnectIn/Global.asax.cs
+++ ConnectInSolution/ConnectIn/Global.asax.cs
@@ -18,7 +18,7 @@ namespace ConnectIn
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            Database.SetInitializer(new DatabaseInitializer());
+            //Database.SetInitializer(new DatabaseInitializer());
         }
     }
 }
