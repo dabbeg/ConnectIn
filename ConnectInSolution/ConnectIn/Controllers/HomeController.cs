d496aff6fb0f8c5682aab9578c03ddc31b1d68d3
Jan <jan14@ru.is>
2015-05-03 16:47:56 +0000

    Created a view for the news feed with dummy data.

--- ConnectInSolution/ConnectIn/Controllers/HomeController.cs
+++ ConnectInSolution/ConnectIn/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ConnectIn.Models.Entity;
 
 namespace ConnectIn.Controllers
 {
@@ -13,6 +14,24 @@ namespace ConnectIn.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult NewsFeed()
+        {
+            List<Post> NewsFeed = new List<Post>();
+            Post post1 = new Post();
+            post1.text = "Er jarðskjálft­inn varð í Nepal fyr­ir viku var níu ára göm­ul stúlka, sem dýrkuð er sem gyðja, að und­ir­búa sig fyr­ir að taka á móti til­biðjend­um á heim­ili sínu sem stend­ur við Dur­bar-torgið í Kat­mandú.";
+            post1.ID = 1;
+            post1.userID = "1";
+            NewsFeed.Add(post1);
+
+            Post post2 = new Post();
+            post2.text = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.";
+            post2.ID = 2;
+            post2.userID = "2";
+            NewsFeed.Add(post2);
+
+            return View(NewsFeed);
         }
 
         public ActionResult About()
