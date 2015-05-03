ea6d8a0dd0df5bf4d9fceffccb7caa5fb57a3d23
Davíð Guðni <davidgh13@ru.is>
2015-05-03 15:33:45 -0700

    Added functionality to the initlizer so it disconnects all connections to it when it initilizes. Now it is possible to change column and table names and create new one at will.

--- ConnectInSolution/ConnectIn/DAL/ConnectInInitializer.cs
+++ ConnectInSolution/ConnectIn/DAL/ConnectInInitializer.cs
@@ -10,6 +10,17 @@ namespace ConnectIn.DAL
 {
     public class ConnectInInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
     {
+        // A trick to disconnect all connections to the database to the database can be dropped.
+        // Basically, just switching from multi-user to single-user and back clears the connections.
+        public override void InitializeDatabase(ApplicationDbContext context)
+        {
+            context.Database.ExecuteSqlCommand(TransactionalBehavior.DoNotEnsureTransaction
+                , string.Format("ALTER DATABASE [{0}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE", context.Database.Connection.Database));
+
+            base.InitializeDatabase(context);
+        }
+
+        // Seeds the new database with some hardcoded data
         protected override void Seed(ApplicationDbContext context)
         {
             var users = new List<User>
