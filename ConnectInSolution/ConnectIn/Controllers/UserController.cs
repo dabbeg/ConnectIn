ea16c3bc78c08f6534d40d5cc15c0ed47660d6a3
LogiGudmann <logi@gudmann.is>
2015-05-04 10:35:08 +0000

    Added UserController, added profilephoto uploads to profile, without the database saving function

---
+++ ConnectInSolution/ConnectIn/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Owin;
+using ConnectIn.Models;
+
+namespace ConnectIn.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
