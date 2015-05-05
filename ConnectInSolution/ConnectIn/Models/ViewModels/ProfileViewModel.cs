27647a670e03f312b71c52b6499deec7c7213136
Davíð Guðni <davidgh13@ru.is>
2015-05-05 13:18:38 +0000

    Finished making the profile view work with database data

---
+++ ConnectInSolution/ConnectIn/Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConnectIn.Models.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public List<PostsViewModel> Posts { get; set; }
+        public UserViewModel User { get; set; }
+    }
+}
--- ConnectInSolution/ConnectIn/Models/ViewModels/UserViewModel.cs
+++ ConnectInSolution/ConnectIn/Models/ViewModels/UserViewModel.cs
@@ -10,5 +10,10 @@ namespace ConnectIn.Models.ViewModels
         public string UserId { get; set; }
         public string UserName { get; set; }
         public string ProfilePicture { get; set; }
+        public string Gender { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Work { get; set; }
+        public string School { get; set; }
+        public string Address { get; set; }
     }
 }
