bea663597117a0e361c2eceb290fa0e194a4bf50
Davíð Guðni <davidgh13@ru.is>
2015-05-12 19:02:10 +0000

    Fixed the show more or show less feature

--- ConnectInSolution/ConnectIn/Scripts/myScript.js
+++ ConnectInSolution/ConnectIn/Scripts/myScript.js
@@ -438,30 +438,23 @@ $(document).ready(function () {
         }
     });
 
-    $(function () {
-        $("div.ellipsis-text").dotdotdot({
-            after: "a.more",
-            callback: dotdotdotCallback
-        });
-        $("div.ellipsis-text").on("click", "a", function () {
-            if ($(this).text() == "See more") {
-                var div = $(this).closest("div.ellipsis-text");
-                div.trigger("destroy").find("a.more").hide();
-                div.css("max-height", "");
-                $("a.less", div).show();
-            }
-            else {
-                $(this).hide();
-                $(this).closest("div.ellipsis-text").css("max-height", "100px").dotdotdot({ after: "a.more", callback: dotdotdotCallback });
-            }
-        });
 
-        function dotdotdotCallback(isTruncated, originalContent) {
-            if (!isTruncated) {
-                $("a", this).remove();
-            }
+    $("div .ellipsis-text pre").css("height", "70px").css("overflow", "hidden");
+
+    $("div .ellipsis-text").on("click", "span", function () {
+        var parent = $(this).siblings("pre");
+        var height = parent[0].scrollHeight;
+        if ($(this).hasClass("more")) {
+            $(this).siblings("pre").animate({ height: height }, { duration: 1000 });
+            $(this).hide();
+            $(this).siblings(".less").show();
+        } else {
+            $(this).siblings("pre").animate({ height: "70px" }, { duration: 1000 });
+            $(this).hide();
+            $(this).siblings(".more").show();
         }
     });
+
 
     function goBack() {
         history.go(-1);
