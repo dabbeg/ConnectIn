10680dcff7d74b8f67d13d64d9198bba1721deed
Davíð Guðni <davidgh13@ru.is>
2015-05-12 10:18:02 +0000

    Croping cover photos now works as well

--- ConnectInSolution/ConnectIn/Controllers/PhotoController.cs
+++ ConnectInSolution/ConnectIn/Controllers/PhotoController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ConnectIn.DAL;
@@ -9,10 +8,7 @@ using ConnectIn.Models.ViewModels;
 using ConnectIn.Services;
 using Microsoft.Ajax.Utilities;
 using Microsoft.AspNet.Identity;
-using System.Net;
 using ConnectIn.Utilities;
-using System.Configuration;
-using System.IO;
 using System.Globalization;
 
 namespace ConnectIn.Controllers
@@ -116,15 +112,9 @@ namespace ConnectIn.Controllers
             var context = new ApplicationDbContext();
             var userService = new UserService(context);
 
-            var oldProfilePicture = userService.GetProfilePicture(User.Identity.GetUserId());
-            if (oldProfilePicture.PhotoBytes != null)
-            {
-                context.Photos.Remove(oldProfilePicture);
-            }
-            context.SaveChanges();
-
             return View(userService.GetPhotoById(photoId));
         }
+
         public ActionResult PickCoverPhoto(FormCollection collection)
         {
             string id = collection["photoId2"];
@@ -139,34 +129,34 @@ namespace ConnectIn.Controllers
                 return View("Error");
             }
 
-            int Coverid = Int32.Parse(id);
+            int photoId = Int32.Parse(id);
 
             var context = new ApplicationDbContext();
             var userService = new UserService(context);
 
-            var oldCoverPhoto = userService.GetCoverPhoto(User.Identity.GetUserId());
-            if (oldCoverPhoto != null)
-            {
-                oldCoverPhoto.IsCoverPhoto = false;
-            }
-            var photo = userService.GetPhotoById(Coverid);
-            photo.IsCoverPhoto = true;
-            context.SaveChanges();
-
-            return RedirectToAction("Profile", "Home", new { id = User.Identity.GetUserId() });
+            return View(userService.GetPhotoById(photoId));
         }
 
         [HttpPost]
         public virtual ActionResult CropImage(FormCollection collection)
         {
+            string location = collection["location"];
             string sphotoId = collection["photoId"];
             string scropPointX = collection["cropPointX"];
             string scropPointY = collection["cropPointY"];
             string simageCropWidth = collection["imageCropWidth"];
             string simageCropHeight = collection["imageCropHeight"];
-            if (sphotoId.IsNullOrWhiteSpace() || scropPointX.IsNullOrWhiteSpace() || scropPointY.IsNullOrWhiteSpace() || simageCropWidth.IsNullOrWhiteSpace() || simageCropHeight.IsNullOrWhiteSpace())
+            if (location.IsNullOrWhiteSpace() || sphotoId.IsNullOrWhiteSpace() || scropPointX.IsNullOrWhiteSpace()
+                || scropPointY.IsNullOrWhiteSpace() || simageCropWidth.IsNullOrWhiteSpace() || simageCropHeight.IsNullOrWhiteSpace())
             {
-                return RedirectToAction("test", new { photoId = Int32.Parse(sphotoId) });
+                if (location == "profile")
+                {
+                    return RedirectToAction("PickProfilePicture", new {photoId = Int32.Parse(sphotoId)});
+                }
+                else if(location == "cover")
+                {
+                    return RedirectToAction("PickCoverPhoto", new { photoId = Int32.Parse(sphotoId) });
+                }
             }
 
             int cropPointX = Int32.Parse(scropPointX);
@@ -176,20 +166,44 @@ namespace ConnectIn.Controllers
 
             var context = new ApplicationDbContext();
             var photoService = new PhotoService(context);
+            var userService = new UserService(context);
 
+            // Crop photo according to the parameters
             var photo = photoService.GetPhotoById(Int32.Parse(sphotoId));
             byte[] imageBytes = photo.PhotoBytes;
             byte[] croppedImage = ImageHelper.CropImage(imageBytes, cropPointX, cropPointY, imageCropWidth, imageCropHeight);
 
+            // Create a photo instance for the new cropped photo
             var croppedPhoto = new Photo()
             {
                 UserId = photo.UserId,
                 Date = DateTime.Now,
                 PhotoBytes = croppedImage,
-                ContentType = photo.ContentType,
-                IsProfilePicture = true,
-                IsCoverPhoto = false
+                ContentType = photo.ContentType
             };
+
+            // Profilepicture or coverPicture
+            if (location == "profile")
+            {
+                var profilePicture = userService.GetProfilePicture(User.Identity.GetUserId());
+                if (profilePicture.PhotoBytes != null)
+                {
+                    context.Photos.Remove(profilePicture);
+                }
+                croppedPhoto.IsProfilePicture = true;
+                croppedPhoto.IsCoverPhoto = false;
+            }
+            else if (location == "cover")
+            {
+                var coverPicture = userService.GetCoverPhoto(User.Identity.GetUserId());
+                if (coverPicture != null)
+                {
+                    context.Photos.Remove(coverPicture);
+                }
+                croppedPhoto.IsProfilePicture = false;
+                croppedPhoto.IsCoverPhoto = true;
+            }
+
             context.Photos.Add(croppedPhoto);
             context.SaveChanges();
 
--- ConnectInSolution/ConnectIn/Services/UserService.cs
+++ ConnectInSolution/ConnectIn/Services/UserService.cs
@@ -289,6 +289,8 @@ namespace ConnectIn.Services
             // Create a list of all photos from the user
             var list = (from up in db.Photos
                         where up.UserId == userId
+                        && up.IsProfilePicture == false
+                        && up.IsCoverPhoto == false
                         orderby up.Date descending
                         select up).ToList();
 
